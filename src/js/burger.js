811554754c5e01b30c9b6594e519c68e21931fab
Mark Dumler <3162mol@gmail.com>
2024-10-22 01:04:00 +0300

    burger is ready

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,8 +1,16 @@
 const burgerButton = document.querySelector('.burger')
+const burgerMenu = document.querySelector('.burger-menu')
+const burgerHide = document.querySelector('.burger-hide')
 
 burgerButton.addEventListener('click', function () {
   this.classList.toggle('active')
-  document.querySelector('burger-menu').classList.toggle('open')
+  burgerMenu.classList.toggle('open')
+
+  if (burgerHide.classList.contains('burger-hide')) {
+    burgerHide.classList.remove('burger-hide')
+  } else {
+    burgerHide.classList.add('burger-hide')
+  }
 })
 
 export default burgerButton
