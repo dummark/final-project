e0f8050edc08e9c3bba8026767785d0b56711782
Mark Dumler <3162mol@gmail.com>
2024-10-24 23:05:37 +0300

    доработаны бургер и обратная связь

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,11 +2,21 @@ const burgerButton = document.querySelector('.burger')
 const burgerMenu = document.querySelector('.burger-menu')
 const burgerHide = document.querySelector('.burger-hide')
 const burgerElements = document.querySelector('.burger-elements')
+const overlay = document.createElement('div')
+
+overlay.classList.add('overlay')
+document.body.appendChild(overlay)
 
 burgerButton.addEventListener('click', function () {
   this.classList.toggle('active')
   burgerElements.classList.toggle('active')
   burgerMenu.classList.toggle('open')
+
+  if (feedbackForm.classList.contains('open')) {
+    overlay.style.display = 'block'
+  } else {
+    overlay.style.display = 'none'
+  }
 
   if (burgerHide.classList.contains('burger-hide')) {
     burgerHide.classList.remove('burger-hide')
